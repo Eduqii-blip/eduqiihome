4da7e7e680feb5e7f4c211894422a54e3f8bbd76
Dwamena Bernard Hakeem <64783115+DwamenaHakim@users.noreply.github.com>
2022-04-17 08:21:49 +0000

    refactor

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eduqiihome.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
